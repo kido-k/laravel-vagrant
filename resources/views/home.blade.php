0de2d77e45d175a4524624bb841175d092f7df71
kido-k <kotaro.k13@gmail.com>
2019-03-21 16:59:49 +0900

    update for laradock

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,6 +7,14 @@
     <title>Document</title>
 </head>
 <body>
-    こんにちは！
+こんにちは！<br>
+@if (Auth::check())
+    {{\Auth::user()->name}}さん<br>
+    <a href="/auth/logout">ログアウト</a>
+@else
+    ゲストさん<br>
+    <a href="/auth/login">ログイン</a>
+    <a href="/auth/register">会員登録</a>
+@endif
 </body>
 </html>
